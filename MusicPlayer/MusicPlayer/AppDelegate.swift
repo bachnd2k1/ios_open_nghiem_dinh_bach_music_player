81dedd4dff0372aa5b1a1754065d9140bc31daaa
Bach <motirelia612@gmail.com>
2023-08-24 10:55:35 +0700

    init

---
+++ MusicPlayer/MusicPlayer/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MusicPlayer
+//
+//  Created by Bach Nghiem on 24/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Bach Nghiem on 24/08/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
