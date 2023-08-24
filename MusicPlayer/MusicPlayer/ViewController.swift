7bbbb544eb5e703a4a6bac6bd9913e292f1d0b3f
Bach <motirelia612@gmail.com>
2023-08-24 14:34:10 +0700

    Build SimpleView

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     }
