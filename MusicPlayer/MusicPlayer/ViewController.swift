72cbaea3e02fa0ea773f79698da150a9e6d7161d
Bach <motirelia612@gmail.com>
2023-08-28 08:26:52 +0700

    Build UIControls

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    @IBOutlet private weak var playButton: UIButton!
+    private var isPlay = true
     override var preferredStatusBarStyle: UIStatusBarStyle {
           return .lightContent
     }
         
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction private func handleButtonPlay(_ sender: Any) {
+        isPlay.toggle()
+        let symbolName = isPlay ? "pause.circle.fill" : "play.circle.fill"
+        playButton.setImage(UIImage(systemName: symbolName), for: .normal)
     }
 }
 
